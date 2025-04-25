70c73c51417689a4d10d56e7e2810450f8847c1a
Joshhhuaaa <41166025+Joshhhuaaa@users.noreply.github.com>
2025-04-25 15:16:02 -0500

    Improved build script, modified Engine.dll for saves, Enhanced.ini

--- Build.cmd
+++ Build.cmd
@@ -1,33 +1,44 @@
 @echo off
-setlocal
 
-set sysDir=%~dp0System
+:: Create Inc folders if they don't exist
+if not exist .\Core\Inc\ mkdir .\Core\Inc
+if not exist .\Echelon\Inc\ mkdir .\Echelon\Inc
+if not exist .\EchelonCharacter\Inc\ mkdir .\EchelonCharacter\Inc
+if not exist .\EchelonEffect\Inc\ mkdir .\EchelonEffect\Inc
+if not exist .\EchelonGameObject\Inc\ mkdir .\EchelonGameObject\Inc
+if not exist .\EchelonHUD\Inc\ mkdir .\EchelonHUD\Inc
+if not exist .\EchelonIngredient\Inc\ mkdir .\EchelonIngredient\Inc
+if not exist .\EchelonMenus\Inc\ mkdir .\EchelonMenus\Inc
+if not exist .\EchelonPattern\Inc\ mkdir .\EchelonPattern\Inc
+if not exist .\Editor\Inc\ mkdir .\Editor\Inc
+if not exist .\Engine\Inc\ mkdir .\Engine\Inc
+if not exist .\UDebugMenu\Inc\ mkdir .\UDebugMenu\Inc
+if not exist .\UWindow\Inc\ mkdir .\UWindow\Inc
 
-echo Deleting UnrealScript packages...
-
-del "%sysDir%\Echelon.u"
-del "%sysDir%\EchelonIngredient.u"
-del "%sysDir%\EchelonCharacter.u"
-del "%sysDir%\EchelonHUD.u"
-del "%sysDir%\EchelonPattern.u"
-del "%sysDir%\EchelonMenus.u"
+del ".\System\Echelon.u"
+del ".\System\EchelonIngredient.u"
+del ".\System\EchelonCharacter.u"
+del ".\System\EchelonHUD.u"
+del ".\System\EchelonPattern.u"
+del ".\System\EchelonMenus.u"
 
 :: Not working yet, currently using v1.3
-:: del "%sysDir%\Engine.u"
-:: del "%sysDir%\UWindow.u"
+:: del ".\System\Engine.u"
+:: del ".\System\UWindow.u"
 
 :: Not needed yet, currently using v1.3
-:: del "%sysDi%\Core.u"
-:: del "%sysDir%\EchelonEffect.u"
-:: del "%sysDir%\EchelonGameObject.u"
-:: del "%sysDir%\Editor.u"
-:: del "%sysDir%\UDebugMenu.u"
+:: del ".\System\Core.u"
+:: del ".\System\EchelonEffect.u"
+:: del ".\System\EchelonGameObject.u"
+:: del ".\System\Editor.u"
+:: del ".\System\UDebugMenu.u"
 
-timeout /t 1 >nul
+echo Compiling scripts for Enhanced SC
 
-echo Building UnrealScript packages...
-"%sysDir%\UCC.exe" make
+cd .\System\
+UCC.exe make -nobind
+cd ..
 
-timeout /t 1 >nul
+echo Finished compiling Enhanced SC
 
-endlocal
+PAUSE
--- BuildAll.cmd
+++ BuildAll.cmd
@@ -1,29 +1,40 @@
 @echo off
-setlocal
 
-set sysDir=%~dp0System
+:: Create Inc folders if they don't exist
+if not exist .\Core\Inc\ mkdir .\Core\Inc
+if not exist .\Echelon\Inc\ mkdir .\Echelon\Inc
+if not exist .\EchelonCharacter\Inc\ mkdir .\EchelonCharacter\Inc
+if not exist .\EchelonEffect\Inc\ mkdir .\EchelonEffect\Inc
+if not exist .\EchelonGameObject\Inc\ mkdir .\EchelonGameObject\Inc
+if not exist .\EchelonHUD\Inc\ mkdir .\EchelonHUD\Inc
+if not exist .\EchelonIngredient\Inc\ mkdir .\EchelonIngredient\Inc
+if not exist .\EchelonMenus\Inc\ mkdir .\EchelonMenus\Inc
+if not exist .\EchelonPattern\Inc\ mkdir .\EchelonPattern\Inc
+if not exist .\Editor\Inc\ mkdir .\Editor\Inc
+if not exist .\Engine\Inc\ mkdir .\Engine\Inc
+if not exist .\UDebugMenu\Inc\ mkdir .\UDebugMenu\Inc
+if not exist .\UWindow\Inc\ mkdir .\UWindow\Inc
 
-echo Deleting UnrealScript packages...
+del ".\System\Core.u"
+del ".\System\Engine.u"
+del ".\System\Editor.u"
+del ".\System\EchelonEffect.u"
+del ".\System\Echelon.u"
+del ".\System\EchelonGameObject.u"
+del ".\System\EchelonIngredient.u"
+del ".\System\EchelonCharacter.u"
+del ".\System\EchelonHUD.u"
+del ".\System\EchelonPattern.u"
+del ".\System\UWindow.u"
+del ".\System\UDebugMenu.u"
+del ".\System\EchelonMenus.u"
 
-del "%sysDi%\Core.u"
-del "%sysDir%\Engine.u"
-del "%sysDir%\Editor.u"
-del "%sysDir%\EchelonEffect.u"
-del "%sysDir%\Echelon.u"
-del "%sysDir%\EchelonGameObject.u"
-del "%sysDir%\EchelonIngredient.u"
-del "%sysDir%\EchelonCharacter.u"
-del "%sysDir%\EchelonHUD.u"
-del "%sysDir%\EchelonPattern.u"
-del "%sysDir%\UWindow.u"
-del "%sysDir%\UDebugMenu.u"
-del "%sysDir%\EchelonMenus.u"
+echo Compiling scripts for Enhanced SC
 
-timeout /t 1 >nul
+cd .\System\
+UCC.exe make -nobind
+cd ..
 
-echo Building UnrealScript packages...
-"%sysDir%\UCC.exe" make
+echo Finished compiling Enhanced SC
 
-timeout /t 1 >nul
-
-endlocal
+PAUSE
