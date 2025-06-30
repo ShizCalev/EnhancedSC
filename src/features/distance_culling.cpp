9e69f8150a34c2bcb1bdea4aea4f48be0309751c
Afevis <ShizCalev@users.noreply.github.com>
2025-06-30 15:47:19 -0600

    missing return

--- src/features/distance_culling.cpp
+++ src/features/distance_culling.cpp
@@ -9,6 +9,7 @@ void DistanceCulling::Initialize() const
     if (!isEnabled)
     {
         spdlog::info("Distance Culling: Feature is disabled in config.");
+        return;
     }
     if (uint8_t* SkeletalLODResult = Memory::PatternScan(g_GameDLLs.Engine, "A8 ?? 0F 84 ?? ?? ?? ?? A8 ?? 0F 85 ?? ?? ?? ?? 83 F9", "Skeletal LOD", NULL, NULL))
     {
